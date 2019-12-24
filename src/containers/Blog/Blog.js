b064ea28cd2049f806c584ff62a64e92d3cb2ee9
Gidd <sunil.gidd@CDSAzure.onmicrosoft.com>
2019-12-24 15:11:19 +0530

    Routing SetUP , Rendering Routes and Components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,81 +1,38 @@
 import React, { Component } from 'react';
+import Posts from '../Blog/Posts/Posts';
+import { Route } from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 // import axios from 'axios';
-import axiosInstance from '../../axios';
+//import axiosInstance from '../../axios';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId:null,
-        error: false
-    }
-
-    componentDidMount () {
-        //axios.get('https://jsonplaceholder.typicode.com/posts') 
-        //above statement returns the post but trying to save them in a const doesnt work
-        // const posts = axios.get('https://jsonplaceholder.typicode.com/posts')  -- wrong
-        //bcoz the axios get is asynchronous and javascript executes synchronously so it does not wait till the response received
-        // so axios uses the promises (a default js object instrouced in ES6)
-        // get method returns the promise , on promise resolved we will stroe it in state / prop
-        
-        // then is method which takes function as the input and executes it once the promise get resolved
-
-        //axios.get('https://jsonplaceholder.typicode.com/postss')
-        // after seeting global axios config base url
-        axiosInstance.get('/posts')
-        .then((response) => {
-            // this.setState({posts: response.data});
-            // console.log(response);
-
-            //194. Transforming Data
-            const posts = response.data.splice(0,4);
-            const updatedPosts = posts.map((post)=> {
-                return {
-                    ...post,
-                    author: 'Sunil'
-                }
-            });
-            
-            this.setState({posts: updatedPosts});
-        })
-        .catch(error => {
-            console.log(error);
-            this.setState({error : true});
-        }); 
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId : id});
-    }
 
     render () {
-       let posts = <p style={{textAlign : 'center'}}>Something went Wrong!!</p>
-            if(!this.state.error){
-                posts = this.state.posts.map(post => {
-                    return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    postClicked={() => this.postSelectedHandler(post.id)}/>;
-                });
-            }
 
         return (
-            <div>
-                <section className="Posts">
-                   {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                
+                {/* <Posts/> */}
+                {/* <Route path="/" render={ () => <h1>Home</h1>}/> */}
+                {/* react checks if the current path starts with the path string here '/' s it will display HOme in http://localhost:3000/new-post as well 
+                so add boolean prop exact for exact math of path else it will see only for the prefix*/}
+              
+                {/* <Route path="/" exact render={ () => <h1>Home</h1>}/>
+                <Route path="/"  render={ () => <h1>Home 2</h1>}/> */}
+
+                <Route path="/" exact component={Posts} />
+                {/* component needs to be a reference to the function or class we want to use 
+                so here simply what we import */}
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import axiosInstance from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css'
+
+class Posts extends Component {
+    state = {
+        posts: [],
+    }
+      
+    componentDidMount () {
+        axiosInstance.get('/posts')
+        .then((response) => {
+           
+            const posts = response.data.splice(0,4);
+            const updatedPosts = posts.map((post)=> {
+                return {
+                    ...post,
+                    author: 'Sunil'
+                }
+            });
+            
+            this.setState({posts: updatedPosts});
+        })
+        .catch(error => {
+            console.log(error);
+           // this.setState({error : true});
+        }); 
+    }
+
+
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId : id});
+    }
+
+    render () {
+
+        let posts = <p style={{textAlign : 'center'}}>Something went Wrong!!</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                postClicked={() => this.postSelectedHandler(post.id)}/>;
+            });
+        }
+
+        return (
+            <section className="Posts">
+                   {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
