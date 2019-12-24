a347ac311826ae907462b51db2917d04b2e94e19
Gidd <sunil.gidd@CDSAzure.onmicrosoft.com>
2019-12-24 16:21:01 +0530

    by default its absolute to make it relative to={props.match.url + '/new'}

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
 import './Post.css';
-import { withRouter } from 'react-router-dom';
-// to make any comp aware if the route can be wrapped it with withRouter
-// the props can be passed to child comps of the routed comp ro to any route
-//another way is to pass the props in comp
-//in posts.js <post {...this.props}
-
-
 
 const post = (props) => {
-    console.log(props);
+    //console.log(props);
     return ( 
     <article className="Post" onClick={props.postClicked}>
         <h1>{props.title}</h1>
@@ -22,4 +15,4 @@ const post = (props) => {
    
 
 
-export default withRouter(post);
+export default post;
